013d1a32dadf1f56996a5196d92a26d1b307eeb8
Abhishek Jain <abhishek1.jain@wdadc.com>
2020-01-29 17:33:23 +0530

    node changes updated

--- routes/users.js
+++ routes/users.js
@@ -11,40 +11,34 @@ const config = require('../config/config');
 // Login Page
 router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 
-// Register Page
-router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
-
-router.get("/edit/:id", function(req, res){
-  User.findById(req.params.id,function(err,results){
-    res.render('edit',{
-      results:results
-    });
-  });
-});
-
-router.post("/edit/:id", function(req, res){
-  let info = {};
-  let errors = [];
-   info.name = req.body.name;
-   info.email = req.body.email;
-
-   let query = {_id:req.params.id}
-
-   User.findByIdAndUpdate(query,info, function (err, result) {
-    var token = jwt.sign({ query}, config.secret, {
+// Login
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email }, function (err, user) {
+  
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
+    
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    
+    var token = jwt.sign({ id: user._id, role:user.role }, config.secret, {
       expiresIn: 86400 // expires in 24 hours
     });
       var decoded = jwt.verify(token,config.secret);
-      if(decoded){
-        console.log("User Updated successfully");
-        errors.push({ msg: 'User Updated successfully' });
-        res.redirect(req.get('referer'));
-      }else{
-        console.log(err);
-      }
-  }); 
-  });
+       if(decoded.role== "admin"){
+          res.redirect(`/dashboard/${token}`);
+       }else if(decoded.role == "user"){
+          res.redirect(`/users/edit/${decoded.id}`);
+       }else{
+         res.send({status:400});
+       }
+   });
   
+});
+
+// Register Page
+router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+
 // Register
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body;
@@ -109,32 +103,39 @@ router.post('/register', (req, res) => {
   }
 });
 
+// Edit Page
+router.get("/edit/:id", function(req, res){
+  User.findById(req.params.id,function(err,results){
+    res.render('edit',{
+      results:results
+    });
+  });
+});
 
-// Login
-router.post('/login', (req, res, next) => {
-  User.findOne({ email: req.body.email }, function (err, user) {
-  
-    if (err) return res.status(500).send('Error on the server.');
-    if (!user) return res.status(404).send('No user found.');
-    
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-    
-    var token = jwt.sign({ id: user._id, role:user.role }, config.secret, {
+router.post("/edit/:id", function(req, res){
+  let info = {};
+  let errors = [];
+   info.name = req.body.name;
+   info.email = req.body.email;
+
+   let query = {_id:req.params.id}
+
+   User.findByIdAndUpdate(query,info, function (err, result) {
+    var token = jwt.sign({ query}, config.secret, {
       expiresIn: 86400 // expires in 24 hours
     });
       var decoded = jwt.verify(token,config.secret);
-       if(decoded.role== "admin"){
-          res.redirect(`/dashboard/${token}`);
-       }else if(decoded.role == "user"){
-          res.redirect(`/users/edit/${decoded.id}`);
-       }else{
-         res.send({status:400});
-       }
-   });
-  
-});
+      if(decoded){
+        console.log("User Updated successfully");
+        errors.push({ msg: 'User Updated successfully' });
+        res.redirect(req.get('referer'));
+      }else{
+        console.log(err);
+      }
+  }); 
+  });
 
+// Delete Page
 router.get('/delete/:id', (req, res) => {
   User.deleteOne({ _id:req.params.id }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
