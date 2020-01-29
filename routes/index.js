1a0b4746644745965334be2d63ed8d115310d6da
Abhishek Jain <abhishek1.jain@wdadc.com>
2020-01-29 15:00:36 +0530

    node file changes

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,24 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const config = require('../config/config');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // Dashboard
-router.get('/dashboard', (req, res) => {
-  var users = User.find({});
+router.get('/dashboard/:token', (req, res) => {
+    decoded = jwt.verify(req.params.token,config.secret);
+     if(decoded.role == "admin"){
+    var users = User.find({});
 	users.exec(function(err,data){
 	if(err) throw err
 		res.render('dashboard', { results:data })
 		  });
-    });
+   	}else{
+        res.send({Status:"FALSE"});
+	}
 
-	
+ });
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -31,13 +31,17 @@ router.post("/edit/:id", function(req, res){
    let query = {_id:req.params.id}
 
    User.findByIdAndUpdate(query,info, function (err, result) {
-    if (err) {
-    console.log(err);
-    } else {
-   console.log("User Updated successfully");
-   res.redirect('/dashboard');
-    }
-  }); 
+    var token = jwt.sign({ query}, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+      var decoded = jwt.verify(token,config.secret);
+      if(decoded){
+        console.log("User Updated successfully");
+        res.redirect(`/dashboard/${decoded.id}`);
+      }else{
+        console.log(err);
+      }
+    }); 
   });
   
 // Register
@@ -115,56 +119,43 @@ router.post('/login', (req, res, next) => {
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
     
-    var token = jwt.sign({ id: user._id }, config.secret, {
+    var token = jwt.sign({ id: user._id, role:user.role }, config.secret, {
       expiresIn: 86400 // expires in 24 hours
     });
-    res.status(200).send({ auth: true, token: token });
+      var decoded = jwt.verify(token,config.secret);
+       if(decoded.role== "admin"){
+          res.redirect(`/dashboard/${token}`);
+       }else if(decoded.role == "user"){
+          res.redirect(`/users/edit/${decoded.id}`);
+       }else{
+         res.send({status:400});
+       }
    });
   
 });
 
-//passport.authenticate('local', {
-  //successRedirect: '/dashboard',
-  //failureRedirect: '/users/login',
-  //failureFlash: true
-//})(req, res, next);
 
 router.get('/delete/:id', (req, res) => {
-  let query = {_id:req.params.id};
-  User.deleteOne(query,function(err){
-    if(err){
-      console.log(err);
-    }else{
-      return res.redirect('/dashboard');
-    }
-  })
-});
 
-router.get('/status/:id', (req, res) => {
-  User.findByIdAndUpdate(req.params.id,function(err,results){
-    res.render('dashboard',{
-      results:results
+  User.deleteOne({ _id:req.params.id }, function (err, user) {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user delete.');
+    
+    var token = jwt.sign({ id: req.params.id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
     });
-  });
+   
+      var decoded = jwt.verify(token,config.secret);
+      console.log(decoded);
+       if(decoded){
+        return res.redirect(`/dashboard/${decoded.id}`);
+       }else{
+         res.send({status:400});
+       }
+     });
+
 });
 
-router.post("/status/:id", function(req, res){
-  let infos = {};
-   
-   info.status = 'inactive';
-
-   let querys = {_id:req.params.id}
-     console/log(querys);
-
-   User.updateOne(querys,infos, function (err, results) {
-    if (err) {
-    console.log(err);
-    } else {
-   console.log("status Updated successfully");
-   res.redirect('/dashboard');
-    }
-  }); 
-  });
 
 // Logout
 router.get('/logout', (req, res) => {
